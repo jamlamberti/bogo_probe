3363595415b1912bfe5af6a420f2d2e28609f581
Jack Lamberti <jal416@lehigh.edu>
2016-03-28 14:44:37 -0400

    Added the set of models to be implemented

---
+++ learner/learner.py
@@ -0,0 +1,12 @@
+class Learner(object):
+    def __init__():
+        pass
+
+    def train(x, y):
+        raise NotImplemented()
+
+    def predict(x):
+        raise NotImplemented()
+
+    def predict_proba(x):
+        raise NotImplemented()
