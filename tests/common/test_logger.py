541afc74b0db845c2b75b179169449126b569cf9
Jack Lamberti <jal416@lehigh.edu>
2016-03-28 13:34:14 -0400

    Added a set of tests for util.py

--- tests/common/test_logger.py
+++ tests/common/test_logger.py
@@ -5,11 +5,15 @@ def test_logger():
     """Test the logger class"""
     import os
     log1 = logger.get_logger("test", "unit_test.log")
+    log2 = logger.get_logger('test2', "unit_test2.log")
     log1.debug("This message is a debug message")
     log1.info("This message is an info message")
     log1.warning("This is a warning message")
     log1.error("This is an error message")
     log1.critical("This is a critical message")
+    log2.debug("Hello World!")
     assert os.path.exists('unit_test.log')
+    assert os.path.exists('unit_test2.log')
     assert os.path.exists('logs/main.log')
     os.remove('unit_test.log')
+    os.remove('unit_test2.log')
---
+++ tests/common/test_util.py
@@ -0,0 +1,16 @@
+import pytest
+
+from common import util
+
+def test_util():
+    import os
+    import errno
+    util.make_dir('tmp')
+    assert os.path.exists('tmp')
+    util.make_dir('tmp')
+    os.rmdir('tmp')
+
+    # Should fail?
+    with pytest.raises(OSError) as err:
+        util.make_dir('/storage')
+        assert err.errno ==  errno.EACCES
