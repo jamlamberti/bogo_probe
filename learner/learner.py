e4c13af7263d85063d864867555a1ae956e10054
Jack Lamberti <jal416@lehigh.edu>
2016-03-29 23:29:35 -0400

    Added in some doc stuff

--- learner/learner.py
+++ learner/learner.py
@@ -1,12 +1,30 @@
+"""Generic Learner Class"""
+
 class Learner(object):
-    def __init__():
+    """
+    A wrapper around all ML classes
+    """
+
+    def __init__(self):
+        """
+        Initialize the classifier with whatever hyperparams you want
+        """
         pass
 
-    def train(x, y):
+    def train(self, train_x, train_y):
+        """
+        Train the classifier
+        """
         raise NotImplemented()
 
     def predict(x):
+        """
+        Return predicted class labels
+        """
         raise NotImplemented()
 
     def predict_proba(x):
+        """
+        Return predicted probabilities
+        """
         raise NotImplemented()
