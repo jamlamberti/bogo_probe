67f7774cf623ad09478bc3968e68c3301ab636f8
Jack Lamberti <jal416@lehigh.edu>
2016-04-05 23:13:05 -0400

    Fix for pep8

--- feature_parser/tokenizer.py
+++ feature_parser/tokenizer.py
@@ -1,10 +1,11 @@
 """A simple tokenizer implementation using stopwords"""
 from nltk.corpus import stopwords
 
+
 def tokenize(msg):
     """Tokenize a string"""
     body = ''.join([x if x.isalnum() else ' ' for x in msg])
     tokens = [word.lower()
-            for word in body.split()
-            if word.lower() not in stopwords.words('english')]
+              for word in body.split()
+              if word.lower() not in stopwords.words('english')]
     return tokens
