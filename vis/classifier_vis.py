20e8e09dc62e56d04b816bc35c3c8f3ee1cec9fb
isharmavarun <isharmavarun@yahoo.co.in>
2016-04-20 00:12:50 -0400

    Support for frame name

--- vis/classifier_vis.py
+++ vis/classifier_vis.py
@@ -5,7 +5,7 @@ matplotlib.use('Agg')  # nopep8
 import matplotlib.pyplot as plt
 
 
-def classifier_vis(truth, expected, thresh=0.02, out_file='out.png'):
+def classifier_vis(truth, expected, thresh=0.02, out_file='out.png', frame_name='Ouput Frame'):
     """truth is the black-box model, expected is the surrogate"""
     # slope, intercept, r_value, p_value, std_err
     # = scipy.stats.linregress(truth, expected)
@@ -20,7 +20,8 @@ def classifier_vis(truth, expected, thresh=0.02, out_file='out.png'):
     ax.plot([0.5 + thresh, 0.5 + thresh], [0, 1], 'g--')
     ax.scatter(truth, expected)
     ax.plot([0, 1], [intercept, slope + intercept])
-    ax.text(0.1, 0.95, '$r^2$ = %0.4f' % r_value**2)
+    ax.text(0.1, 1.1, 'Frame - %s' % frame_name)
+    ax.text(0.1, 1.05, '$r^2$ = %0.4f' % r_value**2)
     ax.set_xlabel('$p$')
     ax.set_ylabel('$\\mathrm{E}[x]$')
     plt.tight_layout()
