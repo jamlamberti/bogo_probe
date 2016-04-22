ba4343d7ece040f87a10d581c5bdee9125889586
Jack Lamberti <jal416@lehigh.edu>
2016-04-21 23:33:29 -0400

    Added test for np matrices so bug doesn't happen again

--- tests/learner/test_svm.py
+++ tests/learner/test_svm.py
@@ -19,8 +19,11 @@ def test_svm():
             data.append([xarr[i, j], yarr[i, j]])
             target.append(1 if xarr[i, j] <= yarr[i, j] else 0)
     data = np.array(data)
-    learner.train(data, target)
-    assert learner.predict([1, 0]) == 0
-    assert learner.predict([0, 1]) == 1
-    assert all([abs(1. - i) < 1e-5
-                for i in np.sum(learner.predict_proba(data), axis=1)])
+    for _ in range(2):
+        # Test both with lists and matrices
+        learner.train(data, target)
+        assert learner.predict([1, 0]) == 0
+        assert learner.predict([0, 1]) == 1
+        assert all([abs(1. - i) < 1e-5
+                    for i in np.sum(learner.predict_proba(data), axis=1)])
+        target = np.asmatrix(target)
