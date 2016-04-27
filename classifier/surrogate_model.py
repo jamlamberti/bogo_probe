44670372540b40d04d6ad70becb0ba3025fac836
Jack Lamberti <jal416@lehigh.edu>
2016-04-27 02:00:28 -0400

    Added a commit stage for learn/unlearn function... test for improvement and only commit to data set it we get an improvement

--- classifier/surrogate_model.py
+++ classifier/surrogate_model.py
@@ -29,12 +29,42 @@ def main(
     # We don't really care about the class label
     # probe_y = np.concatenate(y_bins[2:-1], axis=0)
 
-    for iteration in range(iterations):
+    prob_orig_ml = black_box.predict_proba(probe_x)[:, 0]
 
-        prob_orig_ml = black_box.predict_proba(probe_x)[:, 0]
+    for iteration in range(iterations):
         prob_ded_ml = surrogate.predict_proba(probe_x)[:, 0]
         scores = np.abs(prob_orig_ml - prob_ded_ml)
-        worst = np.argmax(scores)
+        # Indexes greater than threshhold
+        indexes = sorted(
+            np.where(scores[:] > threshold)[0],
+            key=lambda x, y=scores: y[x],
+            reverse=True)
+        updated = False
+        for index in indexes:
+            # Try to add it and see if we improve
+            train_hat_p = np.concatenate(
+                (train_hat, probe_x[index, :]), axis=0)
+            train_labs_p = np.concatenate((
+                train_labs,
+                np.asmatrix(
+                    (int(round(prob_orig_ml[index])) + 1) % 2)), axis=0)
+
+            surrogate.train(train_hat_p, train_labs_p)
+
+            updated_scores = np.abs(
+                prob_orig_ml - surrogate.predict_proba(probe_x)[:, 0])
+
+            if np.sum(scores) > np.sum(updated_scores):
+                # Commit the changes
+                train_hat = train_hat_p
+                train_labs = train_labs_p
+                updated = True
+                break
+
+        if not updated:
+            break
+
+        # worst = np.argmax(scores)
 
         ml_util.generate_figure(
             iteration,
@@ -42,17 +72,17 @@ def main(
             surrogate.predict_proba(x_bins[-1])[:, 0],
             out_dir)
 
-        if scores[worst] < threshold:
-            break
+        # if scores[worst] < threshold:
+        #    break
 
-        target_class = (int(round(prob_orig_ml[worst])) + 1) % 2
-        train_hat = np.concatenate((train_hat, probe_x[worst, :]), axis=0)
+        # target_class = (int(round(prob_orig_ml[worst])) + 1) % 2
+        # train_hat = np.concatenate((train_hat, probe_x[worst, :]), axis=0)
 
-        train_labs = np.concatenate(
-            (train_labs, np.asmatrix(target_class)), axis=0)
+        # train_labs = np.concatenate(
+        #    (train_labs, np.asmatrix(target_class)), axis=0)
 
         # Retrain
-        surrogate.train(train_hat, train_labs)
+        # surrogate.train(train_hat, train_labs)
 
     ml_util.generate_figure(
         iterations,
