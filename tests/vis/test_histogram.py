0c1496c17a96c84fb85920f2087bd609dfa7f8b4
Jack Lamberti <jal416@lehigh.edu>
2016-04-27 03:43:15 -0400

    Missed one

--- tests/vis/test_histogram.py
+++ tests/vis/test_histogram.py
@@ -8,7 +8,7 @@ from vis.histogram import histogram_analysis
 def test_histogram():
     """A smoke test"""
     x_range = random.sample(range(1000), 100)
-    xbins = [0, len(xrange)]
+    xbins = [0, len(x_range)]
     histogram_analysis(x_range, xbins, 'tmp.png')
     assert os.path.exists('tmp.png')
     os.remove('tmp.png')
