9dbee90b0d8a89709f39ce42041e2a1ed713631e
Jack Lamberti <jal416@lehigh.edu>
2016-04-27 03:22:31 -0400

    moved away from xrange as it is a python builtin

--- tests/vis/test_histogram.py
+++ tests/vis/test_histogram.py
@@ -1,14 +1,14 @@
 """A collection of tests for histogram.py"""
 
 import os
-import numpy as np
 import random
 from vis.histogram import histogram_analysis
 
+
 def test_histogram():
     """A smoke test"""
-    xrange = random.sample(range(1000), 100)
+    x_range = random.sample(range(1000), 100)
     xbins = [0, len(xrange)]
-    histogram_analysis(xrange, xbins, 'tmp.png')
+    histogram_analysis(x_range, xbins, 'tmp.png')
     assert os.path.exists('tmp.png')
     os.remove('tmp.png')
